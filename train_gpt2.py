135e51ea0defd3c30ad61d6ab5d061ebf19dd1b1
willmason06 <williamjmason06@gmail.com>
2026-07-21 19:58:21 +0100

    initial config

--- train_gpt2.py
+++ train_gpt2.py
@@ -4,6 +4,47 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
+class CasualSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config
+
+
+
+
+
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1)
+        x = x + self.mlp(self.ln_2)
+        return x
+
 
 @dataclass
 class GPTconfig():
@@ -15,6 +56,15 @@ class GPTconfig():
 
 
 class GPT():
+
     def __init__(self, config):
         super.__init__()
         self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wte = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
